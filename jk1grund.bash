38539661a129f16e9ced382448c6f977bb296a31
klausjunker <klaus.junker@gmx.de>
2021-11-05 14:15:13 +0100

    config/todo -  config/jk0-3 i

--- jk1grund.bash
+++ jk1grund.bash
@@ -1,6 +1,8 @@
-#./aptitude/jk0update
-#./aptitude/jk1grund
+./aptitude/jk0update
+./aptitude/jk1grund
 cd ./config
 bash jk0consolesetup
 bash jk1skel
-bash jk2mysqlgast
+bash jk2rootgast
+bash jk3jkbin
+jkaddmysqluser gast gast
