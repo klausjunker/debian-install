05761310afe73a87f5ca3dd61fe8f9189f53237a
klausjunker <klaus.junker@gmx.de>
2021-11-05 18:46:38 +0100

    jk3network in jk1grund

--- jk1.bash
+++ jk1.bash
@@ -2,6 +2,7 @@ cd ./config
 	bash jk0copyjkbin
 	bash jk1consolesetup
 	bash jk2skel
+	bash jk3network
 cd ../aptitude
 	bash jk0aptitude
 	bash jk1mcsshvim
--- jk2.bash
+++ jk2.bash
@@ -1,5 +1,6 @@
 cd .aptitude
 	bash jk3wlan
+exit
 	bash jk8x
 	bash jk9rest
 	bash jk91programmieren
