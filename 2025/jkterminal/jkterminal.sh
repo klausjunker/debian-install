f96314ff8e966518b33b3f677c50e2774aee1c23
gast <gast@pc67.asdf>
2025-07-17 10:33:16 +0200

    .

---
+++ 2025/jkterminal/jkterminal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# pc67
+xfce4-terminal \
+  --disable-server \
+  --geometry 93x29+0+0 \
+        --title="H" --working-directory=/ex/hilfe/ \
+  --tab --title "vim"   --working-directory=/etc/vim/ext \
+  --tab --title "tex"   --working-directory=/ex/texmf/tex/latex/ \
+  --tab --title "bin"   --working-directory=/usr/local/bin \
+  --tab --title "tau"   --working-directory=/ex/temp/tausch \
+  --tab --title "K23"   --working-directory=/ex/2324/ka2324/m-ka2324/mathe5/ka3/1 \
+  --tab --title "2425"  --working-directory=/ex/2425 \
---
+++ 2025/jkterminal/jkterminal2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# pc67 jkterminal2
+xfce4-terminal \
+  --disable-server \
+  --geometry 93x29+0+0 \
+        --title="A" --working-directory=/home/gast/hilfe/ \
+  --tab --title "B"  --working-directory=/home/gast/2425 \
