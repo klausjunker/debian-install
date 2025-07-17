baddce6eb33155d11d27724aade73c9c3ba6d858
gast <gast@pc67.asdf>
2025-07-17 09:17:28 +0200

    17.7.2025 jkalias 0.1

---
+++ 2025/jkalias/jkalias.sh
@@ -0,0 +1,8 @@
+# Datei: /home/gast/.bash_aliases
+alias jkalias="vim ~/.local/jkaliases && source ~/.local/jkaliases " 
+alias jktest='echo "jk-test-alias 123 "'
+alias jkp="ping -c2 192.168.178.1 && ping -c2 192.168.178.65 && ping -c2 8.8.8.8 " 
+alias jksound="aplay /usr/share/sounds/alsa/Front_Center.wav"
+alias cdh="cd /home/gast/hilfe/ "
+alias cdbin="cd /home/gast/.local/bin/ "
+alias cdtex="cd ~/texmf/tex/latex/ "
