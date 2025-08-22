3d72695b15f95ece8dcb2aae4bb793939bd6497b
klausjunker <klaus.junker@gmx.de>
2025-08-22 20:12:59 +0200

    ole - ok fix-source ok

--- tools/fix-console.sh
+++ tools/fix-console.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
-clear
 set -e
 
 JKFILE="/etc/default/console-setup"
 JKBACKUP="$JKFILE.backup.$(date +%F-%H%M%S)"
-JKCOMMAND="cat $JKFILE"
-JkFONTFACE="TerminusBold"
+JKCOMMAND="systemctl restart console-setup"
+JKFONTFACE="TerminusBold"
 JKFONTSIZE="14x28"
-
-echo ">>> Backup anlegen: $JKBACKUP"
-cp "$JKFILE" "$JKBACKUP"
-
-echo ">>> Fertig! Backup liegt unter: $JKBACKUP"
-echo
-echo "alt:"
-eval $JKCOMMAND
-echo ">>> Quellen prüfen und fehlende Bereiche ergänzen..."
-sed -i -E "s/^[[:space:]]*FONTFACE=.*/FONTFACE=$JKFONTFACE/" "$JKFILE"
-sed -i -E "s/^[[:space:]]*FONTSIZE=.*/FONTSIZE=$JKFONTSIZE/" "$JKFILE"
-
-
-
-echo "neu:"
+echo "Backup liegt unter: $JKBACKUP"
+sed -i -E "s/^[[:space:]]*FONTFACE=.*/FONTFACE=\"$JKFONTFACE\"/" "$JKFILE"
+sed -i -E "s/^[[:space:]]*FONTSIZE=.*/FONTSIZE=\"$JKFONTSIZE\"/" "$JKFILE"
 eval $JKCOMMAND
--- tools/fix-sources.sh
+++ tools/fix-sources.sh
@@ -4,28 +4,22 @@ set -e
 
 JKFILE="/etc/apt/sources.list"
 JKBACKUP="$JKFILE.backup.$(date +%F-%H%M%S)"
-JKCOMMAND="cat $JKFILE"
+JKCOMMAND="apt update"
 
-echo ">>> Backup anlegen: $JKBACKUP"
 cp "$JKFILE" "$JKBACKUP"
-
 # -----------------------------------------------------------------------------
-        echo ">>> Quellen prüfen und fehlende Bereiche ergänzen..."
-# -----------------------------------------------------------------------------
-
 # non-free-firmware ergänzen, wenn fehlt
-sed -i -E '/^[[:space:]]*deb-src / {/non-free-firmware/! s/(main)(.*)/\1 non-free-firmware \2/ }' "$JKFILE"
-
-
-
-# non-free ergänzen, wenn fehlt
-sed -i -E '/^[[:space:]]*deb-src / { /non-free/! s/(main)(.*)/\1 non-free \2/ }' "$JKFILE"
-
-# contrib ergänzen, wenn fehlt
-sed -i -E '/^[[:space:]]*deb-src / { /contrib/! s/(main)(.*)/\1 contrib \2/ }' "$JKFILE"
-
+sed -i -E '/^[[:space:]]*deb / {/non-free-firmware/! s/(main)(.*)/\1 non-free-firmware\2/ }' "$JKFILE"
+sed -i -E '/^[[:space:]]*deb-src / {/non-free-firmware/! s/(main)(.*)/\1 non-free-firmware\2/ }' "$JKFILE"
 # -----------------------------------------------------------------------------
-
-echo ">>> Fertig! Backup liegt unter: $JKBACKUP"
+# non-free ergänzen, wenn fehlt
+sed -i -E '/^[[:space:]]*deb / { /non-free /! s/(main)(.*)/\1 non-free\2/ }' "$JKFILE"
+sed -i -E '/^[[:space:]]*deb-src / { /non-free /! s/(main)(.*)/\1 non-free\2/ }' "$JKFILE"
+# -----------------------------------------------------------------------------
+# contrib ergänzen, wenn fehlt
+sed -i -E '/^[[:space:]]*deb / { /contrib/! s/(main)(.*)/\1 contrib\2/ }' "$JKFILE"
+sed -i -E '/^[[:space:]]*deb-src / { /contrib/! s/(main)(.*)/\1 contrib\2/ }' "$JKFILE"
+# -----------------------------------------------------------------------------
+echo "Backup liegt unter: $JKBACKUP"
 eval $JKCOMMAND
 
