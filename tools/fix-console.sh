0fcb78d897d69b06e8a6ece7252cfdfe0f711b36
klausjunker <klaus.junker@gmx.de>
2025-08-22 19:02:28 +0200

    ole jetz ok? - fix -sources reihenfolge

--- tools/fix-console.sh
+++ tools/fix-console.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+clear
 set -e
 
 JKFILE="/etc/default/console-setup"
 JKBACKUP="$JKFILE.backup.$(date +%F-%H%M%S)"
 JKCOMMAND="cat $JKFILE"
+JkFONTFACE="TerminusBold"
+JKFONTSIZE="14x28"
 
 echo ">>> Backup anlegen: $JKBACKUP"
 cp "$JKFILE" "$JKBACKUP"
 
-echo ">>> Quellen prüfen und fehlende Bereiche ergänzen..."
-
 echo ">>> Fertig! Backup liegt unter: $JKBACKUP"
+echo
+echo "alt:"
+eval $JKCOMMAND
+echo ">>> Quellen prüfen und fehlende Bereiche ergänzen..."
+sed -i -E '/^[[:space:]]*FONTFACE(.*)/\1 $JKFONTFACE' "$JKFILE"
+sed -i -E '/^[[:space:]]*FONTFACE(.*)/\1 $JKFONTFACE' "$JKFILE"
+
+echo "neu:"
 eval $JKCOMMAND
--- tools/fix-sources.sh
+++ tools/fix-sources.sh
@@ -9,15 +9,21 @@ JKCOMMAND="cat $JKFILE"
 echo ">>> Backup anlegen: $JKBACKUP"
 cp "$JKFILE" "$JKBACKUP"
 
-echo ">>> Quellen prüfen und fehlende Bereiche ergänzen..."
-# contrib ergänzen, wenn fehlt
-sed -i -E '/^[[:space:]]*deb / {/contrib/! s/(main)(.*)/\1 contrib\2/}' "$JKFILE"
+# -----------------------------------------------------------------------------
+        echo ">>> Quellen prüfen und fehlende Bereiche ergänzen..."
+# -----------------------------------------------------------------------------
+
+# non-free-firmware ergänzen, wenn fehlt
+sed -i -E '/^[[:space:]]*deb / {/non-free-firmware/! s/(non-free)(.*)/\1 non-free-firmware\2/}' "$JKFILE"
+
 
 # non-free ergänzen, wenn fehlt
 sed -i -E '/^[[:space:]]*deb / {/non-free[^-]/! s/(contrib|main)(.*)/\1 non-free\2/}' "$JKFILE"
 
-# non-free-firmware ergänzen, wenn fehlt
-sed -i -E '/^[[:space:]]*deb / {/non-free-firmware/! s/(non-free)(.*)/\1 non-free-firmware\2/}' "$JKFILE"
+# contrib ergänzen, wenn fehlt
+sed -i -E '/^[[:space:]]*deb / {/contrib/! s/(main)(.*)/\1 contrib\2/}' "$JKFILE"
+
+# -----------------------------------------------------------------------------
 
 echo ">>> Fertig! Backup liegt unter: $JKBACKUP"
 eval $JKCOMMAND
