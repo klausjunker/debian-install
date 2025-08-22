bef5d01cac1b1b26d8e3ff37ad58d45432d9e7a8
klausjunker <klaus.junker@gmx.de>
2025-08-22 20:26:23 +0200

    version 0.1.0 -> main

--- tools/fix-sources.sh
+++ tools/fix-sources.sh
@@ -4,19 +4,19 @@ set -e
 
 JKFILE="/etc/apt/sources.list"
 JKBACKUP="$JKFILE.backup.$(date +%F-%H%M%S)"
-JKCOMMAND="apt update"
+JKCOMMAND="apt-get update"
 
 cp "$JKFILE" "$JKBACKUP"
 # -----------------------------------------------------------------------------
-# non-free-firmware ergänzen, wenn fehlt
+# non-free-firmware ergänzen, wenn es fehlt
 sed -i -E '/^[[:space:]]*deb / {/non-free-firmware/! s/(main)(.*)/\1 non-free-firmware\2/ }' "$JKFILE"
 sed -i -E '/^[[:space:]]*deb-src / {/non-free-firmware/! s/(main)(.*)/\1 non-free-firmware\2/ }' "$JKFILE"
 # -----------------------------------------------------------------------------
-# non-free ergänzen, wenn fehlt
+# non-free ergänzen, wenn es fehlt
 sed -i -E '/^[[:space:]]*deb / { /non-free /! s/(main)(.*)/\1 non-free\2/ }' "$JKFILE"
 sed -i -E '/^[[:space:]]*deb-src / { /non-free /! s/(main)(.*)/\1 non-free\2/ }' "$JKFILE"
 # -----------------------------------------------------------------------------
-# contrib ergänzen, wenn fehlt
+# contrib ergänzen, wenn es fehlt
 sed -i -E '/^[[:space:]]*deb / { /contrib/! s/(main)(.*)/\1 contrib\2/ }' "$JKFILE"
 sed -i -E '/^[[:space:]]*deb-src / { /contrib/! s/(main)(.*)/\1 contrib\2/ }' "$JKFILE"
 # -----------------------------------------------------------------------------
